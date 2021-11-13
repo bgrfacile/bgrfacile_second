35dcf39106f8c0b0e1d0d6223cee1d9cc5bd1a70
benji_pc <benaja.bendo02@gmailcom>
2021-11-13 10:14:36 +0100

    mise en place de la stack inertia coté serveur

---
+++ resources/views/layouts/dashboard-layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'bgrfacile') }}</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+
+    <!-- Scripts -->
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+</head>
+
+<body class="font-sans antialiased">
+    @yield('content')
+</body>
+
+</html>
